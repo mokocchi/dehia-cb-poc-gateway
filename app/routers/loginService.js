4236552fd230e3b9b4e76dfb68b157ac0a374383
Jose Arcidiacono <josefine2_94@hotmail.com>
2021-03-17 17:56:06 -0300

    fix incorrect key

--- app/routers/loginService.js
+++ app/routers/loginService.js
@@ -43,7 +43,7 @@ router.post('/login', (req, res) => {
             }, (err, take) => {
                 const accessToken = jwt.sign({
                     name: take
-                }, process.env.JWT_SECRET, { expiresIn: "15m", algorithm: "HS256" });
+                }, process.env.JWT_FRONT_SECRET, { expiresIn: "15m", algorithm: "HS256" });
 
                 res.json({
                     accessToken,
