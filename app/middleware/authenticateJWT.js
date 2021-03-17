a3362f8e6513733b3684575411ead36f38c4b5ed
Jose Arcidiacono <josefine2_94@hotmail.com>
2021-03-17 20:28:56 -0300

    remove temporal fix

--- app/middleware/authenticateJWT.js
+++ app/middleware/authenticateJWT.js
@@ -2,13 +2,14 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
 const { errorResponse } = require('../utils')
+const tokens = {};
 const authenticateJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (authHeader) {
         const token = authHeader.split(' ')[1];
 
-        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        jwt.verify(token, process.env.JWT_FRONT_SECRET, (err, user) => {
             if (err) {
                 console.log(err);
                 errorResponse(err, res, 403, "Invalid token", "Forbidden")
@@ -19,10 +20,16 @@ const authenticateJWT = (req, res, next) => {
                 errorResponse(new Error(`Token expired for user ${user.name}`), res, 403, "Token expired", "Please login again");
             } else {
                 req.user = user;
-                const internal_token = jwt.sign({
-                    name: user.name
-                }, process.env.JWT_SECRET, { expiresIn: "15m", algorithm: "HS256" });
-                //req.headers["authorization"] = `Bearer ${internal_token}`;
+                if (!tokens[token]) {
+                   const internal_token = jwt.sign({
+                        name: user.name
+                    }, process.env.JWT_SECRET, { expiresIn: "15m", algorithm: "HS256" });
+                    req.headers["authorization"] = `Bearer ${internal_token}`;
+                    tokens[token] = internal_token;
+                } else {
+                    const internal_token = tokens[token];
+                    req.headers["authorization"] = `Bearer ${internal_token}`;
+                }
                 next();
             }
         });
