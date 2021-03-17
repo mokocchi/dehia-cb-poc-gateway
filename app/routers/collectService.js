2dfdeb570b7ce11ed363c25f2bfc2d2e5dca9b60
Jose Arcidiacono <josefine2_94@hotmail.com>
2021-03-17 09:08:45 -0300

    typo

--- app/routers/collectService.js
+++ app/routers/collectService.js
@@ -4,7 +4,7 @@ var authenticateJWT = require('../middleware/authenticateJWT');
 var { getApi, postApi, deleteApi } = require("../utils")
 var apiAdapter = require("../utils/apiAdapter")
 var API_PREFIX = process.env.API_PREFIX
-const api = apiAdapter(process.env.CB_POC_COLLECT_URL, tru);
+const api = apiAdapter(process.env.CB_POC_COLLECT_URL, true);
 
 router.get(`${API_PREFIX}/collect-status`, authenticateJWT, (req, res) => {
   getApi(api, req, res)
